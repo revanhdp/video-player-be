9240c7b82a8cce4777c1f93f6c1e38bcdadd90bc
Revan <revanzahadiputra2@gmail.com>
2026-05-07 21:37:27 +0700

    first commit

---
+++ src/users/users.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UpdateUserDto } from './dto/update-user.dto';
+import { PrismaService } from '../prisma/prisma.service';
+
+@Injectable()
+export class UsersService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  create(user: any) {
+    this.users.push(user);
+
+    return user;
+  }
+
+  findAll() {
+    return this.users;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} user`;
+  }
+
+  update(id: number, updateUserDto: UpdateUserDto) {
+    return `This action updates a #${id} user`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} user`;
+  }
+}
